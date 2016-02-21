775191239899e41f5b325d5ceb4129b567f53273
Michael Madison <mkm@cocoonventures.com>
2016-02-21 09:21:59 -0600

    alternative primary key setting
    set primary key within table block

--- db/migrate/1_create_usda_food_groups.rb
+++ db/migrate/1_create_usda_food_groups.rb
@@ -1,7 +1,7 @@
 class CreateUsdaFoodGroups < ActiveRecord::Migration
   def change
-    create_table :usda_food_groups, id: false, primary_key: :code do |t|
-      t.string  :code, null: false, index: true, uniq: true
+    create_table :usda_food_groups, id: false, do |t|
+      t.string  :code, null: false, index: true, uniq: true, primary_key: true
       t.string  :description, null: false
     end
   end
